edcc09e5eaa9992b8aae05acd7302312c44cf723
Daniel Antonio Ventura Ramos <danielantonioventuraramos41@gmail.com>
2026-05-27 11:04:05 -0600

    tabla masajista y entrenador terminado

---
+++ pooDaniel/seleccion/Masajista.java
@@ -0,0 +1,44 @@
+package seleccion;
+
+// Hereda de la clase padre
+public class Masajista extends SeleccionFutbol {
+
+    // Atributos específicos encapsulados
+    private String titulacion;
+    private int aniosExperiencia;
+
+    // Constructor
+    public Masajista(int id, String nombre, String apellidos, int edad, String titulacion, int aniosExperiencia) {
+        super(id, nombre, apellidos, edad);
+        this.titulacion = titulacion;
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    // Método propio de la clase
+    public void darMasaje() {
+        System.out.println(getNombre() + " está dando un masaje de recuperación a un jugador.");
+    }
+
+    // POLIMORFISMO: Sobrescritura del método del padre
+    @Override
+    public void realizarActividad() {
+        System.out.println("[Masajista] " + getNombre() + " está preparando los vendajes y la sesión de fisioterapia.");
+    }
+
+    // Getters y Setters (Encapsulamiento)
+    public String getTitulacion() {
+        return titulacion;
+    }
+
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    public void setAniosExperiencia(int aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
+    }
+}
