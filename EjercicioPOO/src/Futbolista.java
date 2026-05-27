774645908e2dd44c6e36603bff7cf13da620ed0f
Camila Antonia Calderon Cortez <cc25003@esfe.agape.edu.sv>
2026-05-27 11:39:34 -0600

    Codigos

--- EjercicioPOO/src/Futbolista.java
+++ EjercicioPOO/src/Futbolista.java
@@ -1,4 +1,57 @@
 package PACKAGE_NAME;
 
-public class Futbolista {
+public class Futbolista extends SeleccionFutbol {
+
+    // ATRIBUTOS
+    private int dorsal;
+    private String demarcacion;
+
+    // CONSTRUCTOR VACIO
+    public Futbolista() {
+    }
+
+    // CONSTRUCTOR CON PARAMETROS
+    public Futbolista(int id, String nombre, String apellidos, int edad,
+                      int dorsal, String demarcacion) {
+
+        super(id, nombre, apellidos, edad);
+
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    // GETTERS Y SETTERS
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getDemarcacion() {
+        return demarcacion;
+    }
+
+    public void setDemarcacion(String demarcacion) {
+        this.demarcacion = demarcacion;
+    }
+
+    // METODOS PROPIOS
+
+    public void jugarPartido() {
+        System.out.println("El futbolista juega el partido.");
+    }
+
+    public void entrenar() {
+        System.out.println("El futbolista entrena.");
+    }
+
+    // POLIMORFISMO
+
+    @Override
+    public void concentrarse() {
+        System.out.println("El futbolista se concentra antes del partido.");
+    }
 }
--- EjercicioPOO/src/Masajista.java
+++ EjercicioPOO/src/Masajista.java
@@ -1,4 +1,54 @@
 package PACKAGE_NAME;
 
-public class Masajista {
+public class Masajista extends SeleccionFutbol {
+
+    // ATRIBUTOS
+    private String titulacion;
+    private int aniosExperiencia;
+
+    // CONSTRUCTOR VACIO
+    public Masajista() {
+    }
+
+    // CONSTRUCTOR CON PARAMETROS
+    public Masajista(int id, String nombre, String apellidos,
+                     int edad, String titulacion,
+                     int aniosExperiencia) {
+
+        super(id, nombre, apellidos, edad);
+
+        this.titulacion = titulacion;
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    // GETTERS Y SETTERS
+
+    public String getTitulacion() {
+        return titulacion;
+    }
+
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    public void setAniosExperiencia(int aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    // METODO PROPIO
+
+    public void darMasaje() {
+        System.out.println("El masajista da un masaje.");
+    }
+
+    // POLIMORFISMO
+
+    @Override
+    public void concentrarse() {
+        System.out.println("El masajista prepara a los jugadores.");
+    }
 }
